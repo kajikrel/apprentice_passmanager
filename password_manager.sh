7e47483752748b413760436dc3be31b6bacdf4bb
kajikrel <edgedesu@icloud.com>
2023-10-05 16:00:57 +0000

    fix#2

--- password_manager.sh
+++ password_manager.sh
@@ -11,13 +11,41 @@ while true; do
 
     case $choice in
         "Add Password")
-            # 新しい情報を追加のための入力を受け取る
-            echo -n "サービス名を入力してください："
-            read service_name
+            while true; do # サービス名の入力ループを追加
+                echo -n "サービス名を入力してください："
+                read service_name
+                
+                while [ -z "$service_name" ]; do #空白の場合、再入力を求める
+       		    echo "サービス名は空白にできません。再度入力してください。"
+                    read service_name
+  	        done
+    
+                if [ -f "$Passfile.gpg" ]; then
+                    decrypted_data=$(gpg --yes --batch --passphrase="$PASSPHRASE" --decrypt "$Passfile.gpg")
+                    if echo "$decrypted_data" | grep -q "^$service_name:"; then
+                        echo "指定したサービス名は既に存在します。別の名前を使用してください。"
+                        continue # サービス名の入力に戻る
+                    else
+                        break # 正しいサービス名が入力されたのでループを抜ける
+                    fi
+                else
+                    break # 暗号化ファイルが存在しない場合もループを抜ける
+                fi
+            done
+
             echo -n "ユーザー名を入力してください："
             read user_name
+            while [ -z "$user_name" ]; do
+                echo "ユーザー名は空白にできません。再度入力してください。"
+                read user_name
+            done
+
             echo -n "パスワードを入力してください："
             read password
+            while [ -z "$password" ]; do
+                echo "パスワードは空白にできません。再度入力してください。"
+                read password
+            done
 
             # 既存の暗号化ファイルが存在する場合は、その内容を復号化しつつ新しい情報を追加
             # ファイルが存在しない場合は、新しい情報のみを追加
